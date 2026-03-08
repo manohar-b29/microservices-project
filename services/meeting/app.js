b509326074f2f96df65f52349bc6d2740d4a058b
EC2 Default User <ec2-user@ip-172-31-45-250.ap-south-1.compute.internal>
2026-03-08 02:01:52 +0000

    meeting file updated

--- services/meeting/app.js
+++ services/meeting/app.js
@@ -1,14 +1,10 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/meetings",(req,res)=>{
- res.json({
-   service:"meeting",
-   status:"running"
- });
+app.get("/meeting", (req, res) => {
+  res.send("Meeting service is running 🚀");
 });
 
-app.listen(3000,()=>{
- console.log("Meeting service running");
+app.listen(3000, () => {
+  console.log("Meeting service running on port 3000");
 });
