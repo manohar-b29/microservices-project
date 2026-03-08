db8bf3c6f2423d502c8b81b025cc3e2b3402c9f6
EC2 Default User <ec2-user@ip-172-31-45-250.ap-south-1.compute.internal>
2026-03-08 02:16:42 +0000

    aahar file updated

--- services/aahar/app.js
+++ services/aahar/app.js
@@ -1,14 +1,10 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/meetings",(req,res)=>{
- res.json({
-   service:"aahar",
-   status:"running"
- });
+app.get("/aahar", (req, res) => {
+  res.send("aahar service is running 🚀");
 });
 
-app.listen(3000,()=>{
- console.log("Meeting service running");
+app.listen(3000, () => {
+  console.log("aahar service running on port 3000");
 });
